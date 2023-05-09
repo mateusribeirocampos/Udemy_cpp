e215aadb4905d91f867276a2b03b18101d4b31ac
mateusribeirocampos <mateusrcampos@gmail.com>
2023-05-09 08:22:32 -0300

    test modulo

--- Test.c
+++ Test.c
@@ -3,15 +3,16 @@
 
 int main()
 {
-    int valorReal, valorModulo10, valorModulo100, valorDivisao10, valorModulor10_10;
+    int valorReal, valorModulo10, valorDivisao10;
+    // int valorModulo100, valorModulor10_10;
 
     printf("Digite o valor desejado: ");
     scanf("%d", &valorReal);
 
     valorModulo10 = valorReal % 10;
     valorDivisao10 = valorReal / 10;
-    //valorModulo100 = valorReal % 100;
-    //valorModulor10_10 = (valorDivisao10 % 10) / 10;
+    // valorModulo100 = valorReal % 100;
+    // valorModulor10_10 = (valorDivisao10 % 10) / 10;
 
     if (valorDivisao10 == 1)
     {
@@ -20,8 +21,8 @@ int main()
 
     printf("\nO valor de a = %d", valorModulo10);
     printf("\nO valor de a = %d", valorDivisao10);
-    //printf("\nO valor de a = %d", valorModulo100);
-    //printf("\nO valor de a = %d", valorModulor10_10);
+    // printf("\nO valor de a = %d", valorModulo100);
+    // printf("\nO valor de a = %d", valorModulor10_10);
 
     return 0;
 }
