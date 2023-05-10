c2548833cbd53c93ea2c7693adf731d1a94587ca
mateusribeirocampos <mateusrcampos@gmail.com>
2023-05-10 19:25:59 -0300

    Adicionando comentários a aula 107 - struct e substruct

--- Struct/Aula107.c
+++ Struct/Aula107.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct Data
+typedef struct Data // Define uma nova estrutura de dados com o nome "Data"
 {
-    int dia;
-    int mes;
-    int ano;
+    int dia; // Define um campo "dia" do tipo inteiro na estrutura
+    int mes; // Define um campo "mes" do tipo inteiro na estrutura
+    int ano; // Define um campo "ano" do tipo inteiro na estrutura
 
-} Data;
+} Data; // Define um alias para a estrutura de dados, facilitando sua declaração posteriormente
 
-struct Aluno
+struct Aluno // Define uma nova estrutura de dados com o nome "Aluno"
 {
-    int id;
-    Data nascimento;
-    char nome[20];
+    int id; // Define um campo "id" do tipo inteiro na estrutura
+    Data nascimento; // Define um campo "nascimento" do tipo "Data" na estrutura
+    char nome[20]; // Define um campo "nome" do tipo array de caracteres com tamanho 20 na estrutura
 };
 
 int main()
 {
 
     // criando um aluno
-    struct Aluno aluno1;
+    struct Aluno aluno1; // Cria uma variável do tipo "Aluno" chamada "aluno1"
 
     // Modificar valores
-    aluno1.id = 15;
-    aluno1.nascimento.ano = 2024;
-    aluno1.nascimento.mes = 5;
-    aluno1.nascimento.dia = 17;
-    strcpy (aluno1.nome, "Isadora Campos");
+    aluno1.id = 15; // Atribui o valor 15 ao campo "id" da variável "aluno1"
+    aluno1.nascimento.ano = 2024; // Atribui o valor 1981 ao campo "ano" da variável "nascimento" dentro da variável "aluno1"
+    aluno1.nascimento.mes = 05; // Atribui o valor 05 ao campo "mes" da variável "nascimento" dentro da variável "aluno1"
+    aluno1.nascimento.dia = 17; // Atribui o valor 25 ao campo "dia" da variável "nascimento" dentro da variável "aluno1"
+    strcpy(aluno1.nome, "Isadora Campos"); // Copia a string "Mateus Campos" para o campo "nome" da variável "aluno1" usando a função "strcpy"
 
-    printf("Nome: %s e ID: %d", aluno1.nome, aluno1.id);
+    printf("Nome: %s e ID: %d", aluno1.nome, aluno1.id); // Imprime na tela o valor dos campos "nome" e "id" da variável "aluno1"
 
-    printf("\nNascido em: %d / %d / %d", aluno1.nascimento.dia, aluno1.nascimento.mes, aluno1.nascimento.ano);
+    printf("\nNascido em: %d / %d / %d", aluno1.nascimento.dia, aluno1.nascimento.mes, aluno1.nascimento.ano); // Imprime na tela o valor dos campos "dia", "mes" e "ano" da variável "nascimento" dentro da variável "aluno1"
 
-    return 0;
+    return 0; // Retorna o valor 0 para indicar que o programa terminou sem erros
 }
