41b716d56530819a2667e841930fda20defccd4d
Mateus Campos <mateusrcampos@gmail.com>
2023-05-21 11:49:56 -0300

    Ex109 finalizado

--- Struct/Aula109.cpp
+++ Struct/Aula109.cpp
@@ -22,5 +22,20 @@ int main() {
 
     cout << "Fruta: " << primeiraFruta->nome << ", cor: " << primeiraFruta->cor;
 
+    //criando uma lista de valores
+    Fruta *ponteiroListaDeFrutas = new Fruta[2];
+
+    //modificando valores
+    ponteiroListaDeFrutas[0].cor = "Vermelho";
+    ponteiroListaDeFrutas[0].nome = "Morango";
+    ponteiroListaDeFrutas[1].cor = "Verde";
+    ponteiroListaDeFrutas[1].nome = "Kiwi";
+
+    // Percorrendo e exibindo
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "\nFruta: " << ponteiroListaDeFrutas[i].nome << ", cor: " << ponteiroListaDeFrutas[i].cor;
+    }
+    
     return 0;
 }
