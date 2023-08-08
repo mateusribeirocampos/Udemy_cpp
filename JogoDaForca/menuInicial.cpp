a30b8f8582e19b2d3fbb48eb893f1958a4dd60c2
mateusribeirocampos <mateusrcampos@gmail.com>
2023-08-08 17:56:06 -0300

    vetor com palavras e aleatoriedade

--- JogoDaForca/menuInicial.cpp
+++ JogoDaForca/menuInicial.cpp
@@ -2,12 +2,33 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-void limpaTela() 
+void limpaTela()
 {
     system("cls");
+}
+
+string retornaPalavraAleatoria()
+{
+    // vetor com palavras disponíveis
+    string palavras[3] = {"Abacaxi", "Manga", "Morango"};
+    int indiceAleatorio = rand() % 3;
+
+    //cout << palavras[indiceAleatorio];
+
+    return palavras[indiceAleatorio];
+}
+
+void jogarSozinho()
+{
+
+    // palavra a ser adivinhada
+    string palavra = retornaPalavraAleatoria();
+
+    cout << "A palavra secreta e >>> " << palavra;
 }
 
 void menuInicial()
@@ -33,10 +54,12 @@ void menuInicial()
         switch (opcao)
         {
         case 1:
-            cout << "JOGO INICIADO";
+            cout << "JOGO INICIADO" << endl;
+            cout << "\n";
+            jogarSozinho();
             break;
         case 2:
-        // Mostra informação do jogo
+            // Mostra informação do jogo
             cout << "Informacoes do jogo";
             break;
         case 3:
@@ -49,6 +72,9 @@ void menuInicial()
 
 int main()
 {
+    // Para gerar numero aletório
+    srand(time(NULL));
+
     menuInicial();
     return 0;
 }
