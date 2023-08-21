098525c6b6fc5bda67d29006fe94ac8a20b3bc17
mateusribeirocampos <mateusrcampos@gmail.com>
2023-08-21 17:55:13 -0300

    Jogo batalha naval - menu inicial e descrição sobre o jogo

---
+++ JogoBatalhaNaval/BatalhaNaval.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void limpaTela()
+{
+    system("cls");
+}
+
+void menuInicial()
+{
+    int opcao = 0;
+
+    while (opcao < 1 || opcao > 3)
+    {
+        cout << "**  JOGO BATALHA NAVAL - 2023 **\n";
+        cout << "**  1 - Jogar\n";
+        cout << "**  2 - Sobre o jogo\n";
+        cout << "**  3 - Sair do jogo\n";
+        cout << "Escolha uma opcao\n>>> ";
+        cin >> opcao;
+
+        limpaTela();
+
+        switch (opcao)
+        {
+        case 1:
+            cout << "\nVocE escolheu a opcao jogar\n";
+            break;
+        case 2:
+            cout << "\nSobre o Jogo\n";
+            limpaTela();
+            cout << "Informacoes sobre o jogo batalha naval\n"; // Exibe informações sobre o jogo
+            cout << "____________________________________________________\n";
+            cout << "|                JOGO BATALHA NAVAL                 |\n";
+            cout << "| Desenvolvido por Mateus Campos em 2023            |\n";
+            cout << "|                                                   |\n";
+            cout << "| Bem-vindo ao Jogo Batalha Naval!                  |\n";
+            cout << "| Neste jogo, vocE estarA em uma missAo para        |\n";
+            cout << "| afundar os navios inimigos.                       |\n";
+            cout << "|                                                   |\n";
+            cout << "| O mapa Eh representado por uma grade, onde vocE   |\n";
+            cout << "| tentarA determinar a posicao para atirar e        |\n";
+            cout << "| verificar se acertou algum navio inimigo.         |\n";
+            cout << "|                                                   |\n";
+            cout << "| Os navios estAo escondidos no mapa e vocE deverA  |\n";
+            cout << "| usar sua estratEgia para afundA-los.              |\n";
+            cout << "|                                                   |\n";
+            cout << "| Boa sorte, Capitao!                               |\n";
+            cout << "|___________________________________________________|\n";
+
+            cout << "\n1 - Voltar ao menu inicial";
+            cout << "\n2 - Sair do jogo";
+            cout << "\nEscolha uma opcao e tecle ENTER: ";
+            cin >> opcao;
+            if (opcao == 1)
+            {
+                menuInicial();
+            }
+            break;
+        case 3:
+            cout << "\n <<<<< Vejo vocE em breve, CAPITAO! >>>> \n";
+            break;
+        }
+    }
+}
+
+int main()
+{
+
+    menuInicial();
+
+    return 0;
+}
