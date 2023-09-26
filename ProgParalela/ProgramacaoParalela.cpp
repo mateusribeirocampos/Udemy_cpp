474e81cef0ddff12896944dce37c6b36e11fbe93
mateusribeirocampos <mateusrcampos@gmail.com>
2023-09-26 09:38:15 -0300

    Programação paralela

---
+++ ProgParalela/ProgramacaoParalela.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <omp.h> //Biblioteca openMP
+using namespace std;
+
+
+//Função que será executada em paralelo
+void calcular_quadrado(int numero){
+    int resultado = numero * numero;
+    cout << "O quadrado de " << numero << " e " << resultado << endl;
+}
+
+
+int main(){
+    // Lista de números de entrada
+    vector<int> numeros = {2, 3, 4, 5, 6};
+
+    #pragma omp parallel for
+    for (size_t i = 0; i < numeros.size(); i++)
+    {
+        calcular_quadrado(numeros[i]);
+    }
+    
+    return 0;
+}
