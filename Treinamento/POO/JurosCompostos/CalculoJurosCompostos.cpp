f841416cb944140fb128c7321bf78863b1e654b9
mateusribeirocampos <mateusrcampos@gmail.com>
2023-10-06 16:31:40 -0300

    Calculo de juros compostos C++ em POO

---
+++ Treinamento/POO/JurosCompostos/CalculoJurosCompostos.cpp
@@ -0,0 +1,49 @@
+#include <iostream> // biblioteca para entrada e saída de dados
+#include <cmath> // biblioteca para funções matemáticas
+#include <iomanip> // biblioteca para manipulação de entrada e saída de dados
+using namespace std; // para não precisar escrever std::cout, std::cin, std::endl, etc.
+
+class CalculoJurosCompostos
+{
+private:
+    double capitalPrincipal = 1000.0; //quantia inicial antes dos juros 
+    double taxa = 0.05; // taxa de juros 
+    double tempo = 10;  // tempo em anos
+    double montante; // quantia em depósito ao fim de cada ano
+
+    public:
+    void setCapitalPrincipal(double capitalPrincipal){
+        this->capitalPrincipal = capitalPrincipal;
+    }
+    void setTaxa(double taxa){
+        this->taxa = taxa;
+    }
+    void setTempo(double tempo){
+        this->tempo = tempo;
+    }
+    double getMontante(){
+        return montante;
+    }
+    // Calcula quantia de depósito para cada um dos dez anos
+    void calcularMontante(){
+        // exibe cabeçalhos
+        cout << "Ano" << setw(25) << "Montante depositado" << endl;
+        // configura o formato de saída de ponto flutuante
+        cout << fixed << setprecision(2);
+        // calcula quantia de depósito para cada um dos dez anos
+        for (int ano = 1; ano <= tempo; ano++){
+            // calcula nova quantia para o ano especificado
+            montante = capitalPrincipal * pow(1.0 + taxa, ano);
+            // exibe o ano e a quantia
+            cout << setw(4) << ano << setw(21) << montante << endl;
+        }
+    }
+};
+
+int main(){
+
+    CalculoJurosCompostos calculoJurosCompostos;
+    calculoJurosCompostos.calcularMontante();
+
+    return 0;
+}
