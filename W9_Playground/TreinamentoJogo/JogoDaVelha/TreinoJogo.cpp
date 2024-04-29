4160166ea78c720b135d960bcd0ab6d55304befb
mateusribeirocampos <mateusrcampos@gmail.com>
2024-04-29 16:49:18 -0300

    updated

--- W9_Playground/TreinamentoJogo/JogoDaVelha/TreinoJogo.cpp
+++ W9_Playground/TreinamentoJogo/JogoDaVelha/TreinoJogo.cpp
@@ -3,31 +3,108 @@
 #include <vector>
 #define tam 3
 
+void menuInicial();
+
 void limpaTela()
 {
     system("cls");
 }
 
+void preecheTabuleiro(char tabuleiro[tam][tam])
+{
+
+    int linhas, colunas;
+    for (int linhas = 0; linhas < tam; linhas++)
+    {
+        for (int colunas = 0; colunas < tam; colunas++)
+        {
+            tabuleiro[linhas][colunas] = '-';
+        }
+    }
+}
+
+void exibeTabuleiro(char tabuleiro[tam][tam])
+{
+    for (int linhas = 0; linhas < tam; linhas++)
+    {
+        for (int colunas = 0; colunas < tam; colunas++)
+        {
+            std::cout << tabuleiro[linhas][colunas];
+        }
+        std::cout << std::endl;
+    }
+}
+
 void jogar()
 {
     char tabuleiro[tam][tam];
-    int linha, coluna;
+    int linhas, colunas;
+    int linhaJogada, colunaJogada;
+    int turnoJogador = 1;
+    int rodada;
+    int opcaoFimDoJogo;
 
-    for (int linha = 0; linha < tam; linha++)
+    preecheTabuleiro(tabuleiro);
+
+    while (rodada < 9)
     {
-        for(int coluna = 0; coluna < tam; coluna++)
+        exibeTabuleiro(tabuleiro);
+        std::cout << "Rodada: " << rodada << std::endl;
+        std::cout << "Jogador, digite o numero da linha: ";
+        std::cin >> linhaJogada;
+        std::cout << "Jogador, digite o numero da coluna: ";
+        std::cin >> colunaJogada;
+
+        limpaTela();
+
+        if (turnoJogador == 1)
         {
-            tabuleiro[linha][coluna] = '-';
+            tabuleiro[linhaJogada][colunaJogada] = 'X';
+            turnoJogador = 2;
         }
+        else
+        {
+            tabuleiro[linhaJogada][colunaJogada] = 'O';
+            turnoJogador = 1;
+        }
+        rodada++;
     }
-
-    for (int linha = 0; linha < tam; linha++)
+    std::cout << "Fim do Jogo...!" << std::endl;
+    std::cout << "Deseja jogar novamente?" << std::endl;
+    while (opcaoFimDoJogo != 3)
     {
-        for(int coluna = 0; coluna < tam; coluna++)
+        std::cout << "----------------------" << std::endl;
+        std::cout << "1 - Jogar novamente" << std::endl;
+        std::cout << "2 - Voltar ao menu inicial" << std::endl;
+        std::cout << "3 - Sair" << std::endl;
+        std::cout << "----------------------" << std::endl;
+        std::cout << ">> ";
+        std::cin >> opcaoFimDoJogo;
+
+        limpaTela();
+
+        switch (opcaoFimDoJogo)
         {
-            std::cout << tabuleiro[linha][coluna] << "";
+        case 1:
+            jogar();
+            break;
+
+        case 2:
+            menuInicial();
+            break;
+
+        case 3:
+            std::cout << "Saindo do jogo...!" << std::endl;
+            std::exit(0);
+            break;
+
+        default:
+            if (opcaoFimDoJogo != 3)
+            {
+                std::cout << "Opcao Invalida" << std::endl;
+            }
+            break;
         }
-        std::cout << std::endl;
     }
 }
 
@@ -37,12 +114,17 @@ void menuInicial()
 
     while (opcao != 4)
     {
+        std::cout << "---------------------------" << std::endl;
         std::cout << "Jogo da Velha" << std::endl;
         std::cout << "1 - Jogar" << std::endl;
         std::cout << "2 - Ver Ranking" << std::endl;
         std::cout << "3 - Ver Regras e Instrucoes" << std::endl;
         std::cout << "4 - Sair" << std::endl;
+        std::cout << "---------------------------" << std::endl;
+        std::cout << ">> ";
         std::cin >> opcao;
+
+        limpaTela();
 
         switch (opcao)
         {
@@ -58,7 +140,8 @@ void menuInicial()
             break;
 
         case 4:
-            std::cout << "AtE logo!" << std::endl;
+            std::cout << "Saindo do jogo...." << std::endl;
+            std::exit(0);
             break;
 
         default:
